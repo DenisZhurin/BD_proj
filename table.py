1b8b269b24c31dae7b083c9c31de7a9789a327cc
DenisZhurin <129519135+DenisZhurin@users.noreply.github.com>
2023-05-26 19:21:45 +0300

    Update table.py
    add data

--- table.py
+++ table.py
@@ -2,5 +2,5 @@ import pandas as pd
 import streamlit as st
 from bokeh.models.widgets import Div
 st.set_page_config(layout = "wide")
-data = pd.read_csv('data.csv')
+data = pd.read_csv('https://github.com/DenisZhurin/BD_proj/blob/0112ca88c66d310183c0edbe214771923f6ff1ba/data.csv?raw=true')
 st.write(data)
