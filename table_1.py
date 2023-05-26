fd2109e77ca0bcd5fcfdc1a8fbb4b4769c30a9c4
DenisZhurin <129519135+DenisZhurin@users.noreply.github.com>
2023-05-26 19:29:08 +0300

    Update table_1.py
    add link

--- table_1.py
+++ table_1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 st.set_page_config(layout = "wide")
+st.title('Table with calculated values')
 data = pd.read_csv('https://github.com/DenisZhurin/BD_proj/blob/0112ca88c66d310183c0edbe214771923f6ff1ba/data.csv?raw=true')
 data['Price(Unit_price*Quantity)'] = round(data['Quantity']*data['Unit_price($)'], 2) #Кол-во на цену одной позиции
 data['Tax_price'] = round(data['Price(Unit_price*Quantity)']*0.05, 2)
@@ -11,3 +12,9 @@ finish = pd.DataFrame([[str('Result:'), sum(data['Total']), "", "", "", "", "", 
 data = pd.concat([data, sums])
 data = pd.concat([data, finish])
 st.write(data)
+
+if st.button('Return to table'):
+    js = "window.open('https://deniszhurin-bd-proj-table-8n36ft.streamlit.app/')"  # New tab or window
+    html = '<img src onerror="{}">'.format(js)
+    div = Div(text=html)
+    st.bokeh_chart(div)
