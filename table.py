96d057dcc461695ff44d6669631b5d077f82075c
DenisZhurin <129519135+DenisZhurin@users.noreply.github.com>
2023-05-26 19:31:47 +0300

    Update table.py
    fix

--- table.py
+++ table.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import streamlit as st
 from bokeh.models.widgets import Div
-st.title('Table')
 st.set_page_config(layout = "wide")
+st.title('Table')
 data = pd.read_csv('https://github.com/DenisZhurin/BD_proj/blob/0112ca88c66d310183c0edbe214771923f6ff1ba/data.csv?raw=true')
 st.write(data)
 
