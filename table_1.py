4c9ab7813cf92f4aa57bfdd58cb354d0ccccb1bb
DenisZhurin <129519135+DenisZhurin@users.noreply.github.com>
2023-05-26 19:32:57 +0300

    Update table_1.py
    fix

--- table_1.py
+++ table_1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+from bokeh.models.widgets import Div
 st.set_page_config(layout = "wide")
 st.title('Table with calculated values')
 data = pd.read_csv('https://github.com/DenisZhurin/BD_proj/blob/0112ca88c66d310183c0edbe214771923f6ff1ba/data.csv?raw=true')
